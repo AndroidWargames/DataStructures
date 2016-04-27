c7579252e6a9bc47e15974ba82d2f847288806ae
androidwargames <adwagner89@gmail.com>
2016-04-26 22:03:47 -0500

    First run on check_brackets

--- ProgAss1/check_brackets.py
+++ ProgAss1/check_brackets.py
@@ -16,17 +16,34 @@ class Bracket:
             return True
         return False
 
+
 if __name__ == "__main__":
     text = sys.stdin.read()
 
     opening_brackets_stack = []
+    z = -1
+    y = 0
     for i, next in enumerate(text):
+        y += 1
         if next == '(' or next == '[' or next == '{':
-            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, y))
+            z += 1
             pass
 
         if next == ')' or next == ']' or next == '}':
-            # Process closing bracket, write your code here
+            if z == -1:
+                opening_brackets_stack.append(Bracket(next, y))
+                break
+            x = opening_brackets_stack[z]
+            if x.Match(next):
+                opening_brackets_stack.pop()
+            else:
+                opening_brackets_stack.append(Bracket(next, y))
+                break
+            z -= 1
             pass
 
-    # Printing answer, write your code here
+    if len(opening_brackets_stack) == 0:
+        print("Success")
+    else:
+        print(opening_brackets_stack[z].position)
