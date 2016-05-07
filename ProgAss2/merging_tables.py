200d1b9b5c5579a19aa0f7d16566db63d9b5d949
androidwargames <adwagner89@gmail.com>
2016-05-07 10:18:19 -0700

    merge better

--- ProgAss2/merging_tables.py
+++ ProgAss2/merging_tables.py
@@ -4,6 +4,8 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 lines = list(map(int, sys.stdin.readline().split()))
+#n, m = 5, 5
+#lines = [1, 1, 1, 1, 1]
 rank = [1] * n
 parent = list(range(0, n))
 ans = max(lines)
@@ -11,7 +13,7 @@ ans = max(lines)
 
 def getParent(table):
     if parent[table] != table:
-        parent[table] = getParent[parent[table]]
+        parent[table] = getParent(parent[table])
     return parent[table]
 
 
@@ -22,12 +24,13 @@ def merge(destination, source):
         return False
 
     parent[source] = destination
-    rank[destination] += rank[source]
+    rank[realDestination] += rank[realSource]
 
     return True
 
 for i in range(m):
-    destination, source = map(int, sys.stdin.readline().split())
+    # destination, source = map(int, sys.stdin.readline().split())
+    destination, source = [3, 2, 1, 5, 5][i], [5, 4, 4, 4, 3][i]
     merge(destination - 1, source - 1)
 
     print(max(rank))
